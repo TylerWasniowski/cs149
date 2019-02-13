b420d54d2dde36e01b4439d354e5f453cf52250c
TylerWasniowski <TylerWasniowski@gmail.com>
2019-02-13 11:23:51 -0800

    Added complement factors

--- fastfactor.c
+++ fastfactor.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <sys\timeb.h>
 
 int main(int argc, char* args) {
+	/*
+		Used https://cboard.cprogramming.com/c-programming/171428-measuring-time-function-execution-milliseconds-linux-environment.html
+		to figure out how to get system time
+	*/
+	time_t start, stop;
 
-	// // Taken from: https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-	// // Because C lib returned an int
-	// long long gcd_long(long long a, long long b) {
-	// 	if (a < 0)
-	// 		a = -a;
-	// 	if (b < 0)
-	// 		b = -b;
-	// 	if (b)
-	// 		return gcd_long(b, a % b);
-	// 	else
-	// 		return a;
-	// }
+	time(&start);
 
-	long long N = 234121324120;
+	long long N = 9222343223213138933;
 	long long Nsqrt = (long long) sqrt(N);
 	for (long long n = 2; n <= Nsqrt; n++) {
-		if (N % n == 0)
-			printf("%lld\n", n);		
+		if (N % n == 0) {
+			printf("%lld\n", n);
+			printf("%lld\n", N / n);
+		}
 	}
+	
+	time(&stop);
 
-	printf("done\n");
+	printf("done in %is\n", stop - start);
 }
