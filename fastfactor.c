8c985c3d837215ee3a4643dc7561103bb847f9dc
Tyler Wasniowski <TylerWasniowski@gmail.com>
2019-02-14 23:00:52 -0800

    Implemented input

--- fastfactor.c
+++ fastfactor.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <math.h>
 
@@ -14,11 +16,13 @@ void writeFactors(long long N, long long first, long long last, int pipeWriteEnd
 	{
 		if (N % n == 0)
 		{
-			write(pipeWriteEnd, &n, 8);
+			// Learned to use sizeof from: https://stackoverflow.com/a/1788048
+			write(pipeWriteEnd, &n, sizeof(N));
 
+			// If statement here removes dupes
 			long long nDiv = N / n;
-			if (n != nDiv)
-				write(pipeWriteEnd, &nDiv, 8);
+			if (nDiv > last)
+				write(pipeWriteEnd, &nDiv, sizeof(nDiv));
 		}
 	}
 }
@@ -27,11 +31,11 @@ void writeFactors(long long N, long long first, long long last, int pipeWriteEnd
  * Spawn children and have them start factoring and piping the results
  * Terminates children afterwards
  **/
-void computeFactors(int numChildren, long long N, int pipeWriteEnd) {
-	long long Nsqrt = (long long) sqrt(N) + 1;
+void writeFactorsParallel(long long N, int numChildren, pid_t children[], int pipeWriteEnd) {
+	long long Nsqrt = (long long) ceil(sqrt(N));
 	long long Npart = Nsqrt / numChildren;
 
-	long long start = 2;
+	long long start = 1;
 	for (int n = 0; n < numChildren; n++) {
 		long long end = start + Npart;
 		end = end < Nsqrt ? end : Nsqrt;
@@ -41,29 +45,71 @@ void computeFactors(int numChildren, long long N, int pipeWriteEnd) {
 			writeFactors(N, start, end, pipeWriteEnd);
 			exit(0);
 		} else if (pid == -1) {
-			perror("Could not spawn child. Exiting...");
-			exit(0);
+			printf("Could not spawn child number, %d, in writeFactorsParallel(). Exiting...\n", n);
+			exit(-1);
 		}
+		children[n] = pid;
 
 		start = end + 1;
 	}
 }
 
-int main(int argc, char* args)
-{
-	int pipeArr[2];
+void printFactors(long long N, int numChildren) {
+	if (N < 0) {
+		printf("Got negative number, %lld, in printFactors(). Exiting...\n", N);
+		exit(-1);
+	}
 
+	int pipeArr[2];
 	pipe(pipeArr);
 
-	long long N = 9222343223213138933;
+	if (N < 128) {
+		numChildren = 1;
+	}
 
-	computeFactors(8, N, pipeArr[1]);
+	pid_t children[numChildren];
+	writeFactorsParallel(N, numChildren, children, pipeArr[1]);
+
 	// Was told to close the end that I'm not using by https://www.tldp.org/LDP/lpg/node11.html
 	close(pipeArr[1]);
 
+	for (int i = 0; i < numChildren; i++) wait(&children[i]);
+
 	long long factor;
-	while (1) {
-		read(pipeArr[0], &factor, 8);
-		printf("%lld\n", factor);
-	}
+	printf("%lld: ", N);
+	while (read(pipeArr[0], &factor, sizeof(factor))) printf("%lld ", factor);
+	printf("\n");
+}
+
+int main(int argc, char** argv)
+{
+	const int NUM_CHILDREN = 8;
+
+	if (argc == 1) {
+		long long N;
+        int scanResCode = 1;
+        for (int i = 0; scanResCode != EOF; i++) {
+            scanf("%lld\n", &N);
+
+			if (scanResCode -= 0) {
+				printf("Couldn't convert number in main(). Exiting...\n");
+				exit(-1);
+			}
+
+			printFactors(N, NUM_CHILDREN);
+        }
+	} else {
+        for (int i = 1; i < argc; i++) {
+			// Followed this example for strtoll: https://codeforwin.org/2018/01/convert-string-to-long-long-using-strtoll-c.html
+			char* endPtr;
+			long long N = strtoll(argv[i], &endPtr, 10);
+			if (*endPtr) {
+				printf("Couldn't convert number, %s, in main(). Exiting...\n", argv[i]);
+				exit(-1);
+			}
+
+            printFactors(N, NUM_CHILDREN);
+        }
+    }
+
 }
