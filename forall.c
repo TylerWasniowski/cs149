05630441229399132c0950ff48d62b4caf397863
Tyler Wasniowski <TylerWasniowski@gmail.com>
2019-03-05 16:00:12 -0800

    Added intercepting of signal

--- forall.c
+++ forall.c
@@ -1,32 +1,62 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include <errno.h>
 
 #include <math.h>
+#include <string.h>
 
 #include <sys/types.h>
-#include <sys/wait.h>
 
 
 char* fileExtension = ".out";
 
+int stdoutCopy;
+int stderrCopy;
 
-int main(int argc, char** argv)
-{
+void error(int exitCode, char errorText[]) {
+    perror(errorText);
+    fflush(stderr);
+    exit(exitCode);
+}
+
+// Output messages to console
+void restoreConsole() {
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    dup2(stdoutCopy, STDOUT_FILENO);
+    dup2(stderrCopy, STDERR_FILENO);
+}
+
+void signalHandler(int sig, siginfo_t* sigInfo, void* context) {
+    restoreConsole();
+
+    printf("Signalling %d\n", sig);
+    // TODO: signal to child
+    fflush(stdout);
+}
+
+int main(int argc, char* argv[]) {
     if (argc <= 1) {
         printf("No command provided. Exiting...");
         exit(-1);
     }
 
+    stdoutCopy = dup(STDOUT_FILENO);
+    stderrCopy = dup(STDERR_FILENO);
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = signalHandler;
+    sigaction(SIGINT, &sa, NULL);
+
     char* command = argv[1];
     int fileNumber = 1;
-
-    // Will route stdout to the next open file, and stderr to the one after that
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
 
     for (int i = 2; i < argc; i++) {
         int filenameLength = (log(fileNumber) / log(10) + 1) + strlen(fileExtension);
@@ -35,30 +65,42 @@ int main(int argc, char** argv)
         // Learned to use sprintf from here: https://stackoverflow.com/a/5172154
         sprintf(filename, "%d.out", fileNumber);
 
+        // Will route stdout to the next open file, and stderr to the one after that
+        close(STDOUT_FILENO);
+        close(STDERR_FILENO);
         int outFd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
         int errFd = dup(outFd);
 
         fileNumber++;
 
+
         switch (fork()) {
             case 0:
+                sa.sa_sigaction = SIG_IGN;
+                sigaction(SIGINT, &sa, NULL);
+
                 printf("Executing %s %s\n", command, argv[i]);
                 fflush(stdout);
                 execlp(command, command, argv[i], NULL);
-                exit(0);
+
+                printf("Error executing \"%s %s\". Exiting...\n", command, argv[i]);
+                fflush(stdout);
+                error(errno, "exec");
                 break;
             case -1:
-                printf("Error executing input \"%s\". Exiting...\n", argv[i]);
+                printf("Error forking on input \"%s %s\". Exiting...\n", command, argv[i]);
                 fflush(stdout);
-                exit(-1);
+                error(errno, "main fork");
                 break;
         }
 
         int exitCode;
-        wait(&exitCode);
+        pid_t pid;
+        do {
+            pid = wait(&exitCode);
+        } while (errno == EINTR);
+
         printf("Finished executing %s %s exit code = %d\n", command, argv[i], exitCode);
         fflush(stdout);
-        close(outFd);
-        close(errFd);
     }
 }
